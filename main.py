e170d8e16ef6e5acd02b5b6bc57fa0b69d2e118c
raccoon <steppehare@gmail.com>
2026-04-13 17:23:29 +0300

    Add small fixes

--- main.py
+++ main.py
@@ -17,17 +17,17 @@ class Pair(BaseModel):
 
 
 async def get_all_pairs():
-    customers = [
+    pairs = [
         Pair(id=1, name="EURUSD", values=(PairValue.eur, PairValue.usd)),
         Pair(id=2, name="GBPJPY", values=(PairValue.gbp, PairValue.jpy)),
         Pair(id=3, name="USDJPY", values=(PairValue.usd, PairValue.jpy)),
         Pair(id=4, name="USDGBP", values=(PairValue.usd, PairValue.gbp))
     ]
-    return customers
+    return pairs
 
 
 # -----[ Endpoints ]-----
 
-@app.get("/")
+@app.get("/", response_model=list[Pair])
 async def get_pairs():
     return await get_all_pairs()
