bfb75c7899462afc06267c1f97399f5ccf419197
raccoon <steppehare@gmail.com>
2026-04-20 19:42:06 +0300

    Fetch historical data

---
+++ data/fetch.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+from pathlib import Path
+
+
+def main():
+    hist_data = yf.download(
+        "EURUSD=X",
+        start="2023-01-01",
+        end="2026-01-01",
+        interval="1d",
+        auto_adjust=True
+    )
+    hist_data.columns = hist_data.columns.get_level_values(0)  # leave only column headers
+    hist_data.to_csv({Path(__file__).parent} / "hist_data.csv")
+
+
+if __name__ == "__main__":
+    main()
